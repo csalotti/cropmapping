77a0ee271dac388de7d852e40952262937a88a8a
Christopher Salotti <christopher.salotti@gmail.com>
2023-11-29 14:19:18 +0100

    Add temp as feature in data

--- cmap/data/modules.py
+++ cmap/data/modules.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from glob import glob
 from os.path import join
@@ -5,12 +6,15 @@ from typing import List
 
 import pandas as pd
 import pytorch_lightning as L
+import multiprocessing
 import yaml
 from torch.utils.data import DataLoader
 from torch.utils.data.datapipes.iter.combinatorics import ShufflerIterDataPipe
 
 from cmap.data.dataset import ChunkLabeledDataset, ChunkMaskedDataset
-from cmap.utils.chunk import chunks_indexing
+import os
+from glob import glob
+from cmap.utils.chunk import chunks_indexing, preprocessing
 from cmap.utils.constants import LABEL_COL, POINT_ID_COL, SEASON_COL
 
 logger = logging.getLogger("cmap.data.module")
@@ -25,6 +29,8 @@ class SITSDataModule(L.LightningDataModule):
         batch_size: int = 32,
         prepare: bool = False,
         num_workers: int = 3,
+        raw_data_root: str = os.environ.get("RAW_DATA_ROOT", ""),
+        include_temp: bool = False,
     ):
         L.LightningDataModule.__init__(self)
 
@@ -32,12 +38,51 @@ class SITSDataModule(L.LightningDataModule):
         self.val_features_root = val_features_root
         self.batch_size = batch_size
         self.prepare = prepare
+        self.raw_data_root = raw_data_root
         self.num_workers = num_workers
+        self.include_temp = include_temp
+
+    def _prepare_chunk(self, chunk_id):
+        stage = "train" if chunk_id <= 80 else "eval"
+        features_df = pd.read_csv(
+            join(self.raw_data_root, stage, "features", f"chunk_{chunk_id}")
+        )
+        temp_files = []
+        if self.include_temp:
+            temp_files = glob(
+                join(
+                    self.raw_data_root,
+                    "temperatures",
+                    chunk_id,
+                    "*.csv",
+                )
+            )
+
+        indexes, features_df = preprocessing(features_df, temp_files)
+
+        features_df.to_csv(
+            join(self.train_features_root, "features", f"chunk_{chunk_id}")
+        )
+        return indexes
 
     def prepare_data(self) -> None:
         if self.prepare:
-            chunks_indexing(join(self.train_root, "features"), write_csv=True)
-            chunks_indexing(join(self.val_root, "features"), write_csv=True)
+            with multiprocessing.Pool() as pool:
+                indexes_map = pool.map(self._prepare_chunk, range(100))
+
+            train_indexes = []
+            val_indexes = []
+            for i, ind in enumerate(indexes_map):
+                if i <= 80:
+                    train_indexes.extend(ind)
+                else:
+                    val_indexes.extend(ind)
+
+            with open(join(self.train_features_root, "indexes.json"), "r") as f:
+                json.dump(train_indexes, f)
+
+            with open(join(self.val_features_root, "indexes.json"), "r") as f:
+                json.dump(val_indexes, f)
 
     def setup(self, stage: str) -> None:
         raise NotImplementedError("Datamodule subclasses must implement setup")
--- cmap/utils/chunk.py
+++ cmap/utils/chunk.py
@@ -1,42 +1,72 @@
-import re
 from glob import glob
-from os.path import join
+from os.path import join, basename
+from os import listdir
 
 import pandas as pd
 from pandas.io.parquet import json
 
-from cmap.utils.constants import (CHUNK_ID_COL, DATE_COL, POINT_ID_COL,
-                                  SIZE_COL, START_COL)
+from cmap.utils.constants import (
+    CHUNK_ID_COL,
+    DATE_COL,
+    POINT_ID_COL,
+    SIZE_COL,
+    START_COL,
+)
 
 
-def chunks_indexing(chunks_root: str, write_csv: bool = False):
-    files = glob(join(chunks_root, "*.csv"))
-    indexes = []
-    for f in files:
-        chunk_df = pd.read_csv(f)
-        chunk_id = chunk_df[CHUNK_ID_COL].unique()[0]
-        chunk_df = (
-            chunk_df.sort_values([POINT_ID_COL, DATE_COL])
-            .reset_index(drop=True)
-            .drop(columns=[cn for cn in chunk_df.columns if "Unnamed" in cn])
-        )
-        indexes_df = chunk_df[[POINT_ID_COL]].assign(id=chunk_df.index)
-        indexes_df = indexes_df.groupby(POINT_ID_COL).agg(["first", "last"])
-        indexes_df.columns = indexes_df.columns.map("_".join)
-        indexes_df = (
-            # fmt: off
+def get_indexes(chunk_df: pd.DataFrame):
+    chunk_id = chunk_df[CHUNK_ID_COL].unique()[0]
+    chunk_df = (
+        chunk_df.sort_values([POINT_ID_COL, DATE_COL])
+        .reset_index(drop=True)
+        .drop(columns=[cn for cn in chunk_df.columns if "Unnamed" in cn])
+    )
+    indexes_df = chunk_df[[POINT_ID_COL]].assign(id=chunk_df.index)
+    indexes_df = indexes_df.groupby(POINT_ID_COL).agg(["first", "last"])
+    indexes_df.columns = indexes_df.columns.map("_".join)
+    indexes_df = (
+        # fmt: off
             indexes_df
             .assign(chunk_id=chunk_id)
             .assign(size=lambda x: x["id_last"] - x["id_first"])
             .rename(columns={"id_first" : START_COL})
-            # fmt: on
-        )
-        indexes.extend(
-            indexes_df.reset_index()[
-                [POINT_ID_COL, START_COL, SIZE_COL, CHUNK_ID_COL]
-            ].to_dict(orient="records")
-        )
-        chunk_df.to_csv(f, index=False)
+        # fmt: on
+    )
 
-    with open(join(chunks_root, "indexes.json"), "w") as fw:
-        json.dump(indexes, fw)
+    return indexes_df, chunk_df
+
+
+def get_temperatures(temp_files):
+    chunk_data = []
+    # Iterate through each CSV file in the chunk folder
+    for csv_file in temp_files:
+        df = pd.read_csv(csv_file, index_col=0).rename(columns={"id_point": "poi_id"})
+
+        # Extract the date from the file name
+        date = csv_file[:-4]  # Remove the '.csv' extension
+        df["date"] = pd.to_datetime(date)
+
+        # Append the DataFrame to the combined_data DataFrame
+        chunk_data.append(df)
+
+    chunk_temp_df = pd.concat(chunk_data).reset_index()
+
+    return chunk_temp_df
+
+
+def preprocessing(features_df, temp_files=[]):
+    if len(temp_files) > 0:
+        temps_df = get_temperatures(temp_files)
+        features_df = pd.merge(
+            features_df,
+            temps_df,
+            on=["date", "poi_id"],
+            how="inner",
+        )
+    indexes_df, features_df = get_indexes(features_df)
+
+    indexes_json = indexes_df.reset_index()[
+        [POINT_ID_COL, START_COL, SIZE_COL, CHUNK_ID_COL]
+    ].to_dict(orient="records")
+
+    return indexes_json, features_df
