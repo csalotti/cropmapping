04630efa6452e41aff4deea5fd505a679fa583bc
Christopher Salotti <christopher.salotti@gmail.com>
2023-11-30 11:54:13 +0100

    Replace Focal Loss

--- cmap/ml/losses.py
+++ cmap/ml/losses.py
@@ -1,25 +1,62 @@
 import torch
+from torch.autograd import Variable
 import torch.nn.functional as F
 from torch import nn
 
 
 class FocalLoss(nn.Module):
-    """
-    Multi-class Focal Loss
-    """
-
-    def __init__(self, gamma=2, weight=None):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
-        self.weight = weight
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1 - alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
 
     def forward(self, input, target):
-        """
-        input: [N, C], float32
-        target: [N, ], int64
-        """
+        if input.dim() > 2:
+            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
         logpt = F.log_softmax(input, dim=1)
-        pt = torch.exp(logpt)
-        logpt = (1 - pt) ** self.gamma * logpt
-        loss = F.nll_loss(logpt, target, self.weight)
-        return loss
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1 - pt) ** self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+
+# class FocalLoss(nn.Module):
+#     """
+#     Multi-class Focal Loss
+#     """
+#
+#     def __init__(self, gamma=2, weight=None):
+#         super(FocalLoss, self).__init__()
+#         self.gamma = gamma
+#         self.weight = weight
+#
+#     def forward(self, input, target):
+#         """
+#         input: [N, C], float32
+#         target: [N, ], int64
+#         """
+#         logpt = F.log_softmax(input, dim=1)
+#         pt = torch.exp(logpt)
+#         logpt = (1 - pt) ** self.gamma * logpt
+#         loss = F.nll_loss(logpt, target, self.weight)
+#         return loss
