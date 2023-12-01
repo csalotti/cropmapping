1a839aaa41069abfb298310e4983dc1e945d6bf5
Christopher Salotti <christopher.salotti@gmail.com>
2023-12-01 16:06:19 +0100

    Class distribution plot

--- cmap/ml/classifiers.py
+++ cmap/ml/classifiers.py
@@ -1,8 +1,8 @@
 import logging
-from typing import List, Dict
+from typing import List, Dict, Optional
 
+from safetensors.torch import load_model
 import numpy as np
-from numpy.lib import append
 import pytorch_lightning as L
 import seaborn as sns
 import torch
@@ -11,8 +11,10 @@ from torch.optim import Adam
 from torch.optim.lr_scheduler import ExponentialLR, LinearLR, SequentialLR
 from torchmetrics.classification import MulticlassConfusionMatrix, MulticlassF1Score
 
+
 from cmap.ml.losses import FocalLoss
 from cmap.utils.attention import patch_attention, plot_attention
+from cmap.utils.distributions import get_dist_plot
 
 logger = logging.getLogger("cmap.ml.modules")
 # logger.addHandler(logging.FileHandler("cmap.ml.modules.log"))
@@ -33,21 +35,23 @@ class Classifier(L.LightningModule):
         self,
         encoder: nn.Module,
         decoder: nn.Module,
-        class_weights : Dict[str, float],
+        class_weights: Optional[Dict[str, float]] = None,
+        encoder_weights_path: str = "",
         classes: List[str] = DEFAULT_CLASSES,
         min_lr: float = 1e-6,
         max_lr: float = 1e-4,
         gamma: float = 0.99,
         warmup_epochs: int = 10,
         wd: float = 1e-4,
-        get_att : bool = True,
     ):
         super().__init__()
 
         # labels
         self.classes = classes
         self.n_classes = len(classes)
-        self.classes_weights = [class_weights[c] for c in classes]
+        self.classes_weights = torch.FloatTensor(
+            [class_weights[c] for c in classes] if (class_weights is not None) else None
+        )
 
         # Optimizationin
         self.min_lr = min_lr
@@ -57,7 +61,7 @@ class Classifier(L.LightningModule):
         self.wd = wd
 
         # Metrics
-        self.criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor(self.classes_weights))
+        self.criterion = nn.CrossEntropyLoss(weight=self.classes_weights)
         self.train_f1 = MulticlassF1Score(num_classes=len(classes))
         self.val_f1 = MulticlassF1Score(num_classes=len(classes))
         self.train_conf_mat = MulticlassConfusionMatrix(
@@ -72,11 +76,15 @@ class Classifier(L.LightningModule):
         # data
         self.val_emb = []
         self.val_batches = []
-        self.get_att = get_att
+        self.train_labels = []
+        self.val_labels = []
 
         # Layers
         self.encoder = encoder
         self.decoder = decoder
+
+        if encoder_weights_path:
+            load_model(self.encoder, encoder_weights_path)
 
         self.save_hyperparameters(ignore=["encoder", "decoder"])
 
@@ -96,7 +104,7 @@ class Classifier(L.LightningModule):
         f1_score = self.train_f1(y_hat_cls, y)
         self.train_conf_mat.update(y_hat, y)
 
-        logger.debug(batch)
+        # Logging
         self.log_dict(
             {
                 "Losses/train": loss,
@@ -106,10 +114,15 @@ class Classifier(L.LightningModule):
             on_epoch=False,
         )
 
+        # Data
+        # Save Labels
+        if self.current_epoch == 0:
+            self.train_labels.extend(y.cpu().tolist())
+
         return loss
 
     def on_train_epoch_end(self):
-        fig_ = sns.heatmap(
+        cmat_fig = sns.heatmap(
             self.train_conf_mat.compute().cpu().numpy(),
             cmap="magma",
             annot=True,
@@ -121,7 +134,7 @@ class Classifier(L.LightningModule):
 
         self.logger.experiment.add_figure(
             "Confusion matrix/Training",
-            fig_,
+            cmat_fig,
             self.current_epoch,
         )
 
@@ -130,8 +143,19 @@ class Classifier(L.LightningModule):
             {"train": self.train_f1.compute()},
             global_step=self.current_epoch,
         )
+
+        # Label distribution
+        if self.current_epoch == 0:
+            labels_dist_fig = get_dist_plot(self.train_labels)
+            self.logger.experiment.add_figure(
+                "Labels Distribution/Training",
+                labels_dist_fig
+                global_step=self.current_epoch,
+            )
+
         self.train_f1.reset()
         self.train_conf_mat.reset()
+        self.train_labels.clear()
 
     def _get_attention_maps(self, ts, days, mask):
         for l in self.encoder.transformer_encoder.layers:
@@ -166,7 +190,7 @@ class Classifier(L.LightningModule):
         self.val_conf_mat.update(y_hat, y)
 
         # Save embeddings
-        if self.current_epoch % 10 == 0:
+        if self.current_epoch % 10 == 1:
             self.val_emb.append(
                 {
                     "embeddings": y_hat.cpu().numpy(),
@@ -175,7 +199,7 @@ class Classifier(L.LightningModule):
             )
 
         # Save data for attention maps
-        if batch_idx == 0 and self.get_att:
+        if batch_idx == 0:
             attn_maps = self._get_attention_maps(ts, days, mask)
             self.val_batches.append(
                 {
@@ -209,7 +233,7 @@ class Classifier(L.LightningModule):
             global_step=self.current_epoch,
         )
 
-        if self.current_epoch % 10 == 0:
+        if self.current_epoch % 10 == 1:
             emb = np.concatenate([d["embeddings"] for d in self.val_emb], axis=0)
             metadata = [di for d in self.val_emb for di in d["labels"]]
             self.logger.experiment.add_embedding(
@@ -239,7 +263,17 @@ class Classifier(L.LightningModule):
                     self.current_epoch,
                 )
 
-        self.val_f1.reset()
+        # Label distribution
+        if self.current_epoch == 0:
+            labels_dist_fig = get_dist_plot(self.val_labels)
+            self.logger.experiment.add_figure(
+                "Labels Distribution/Validation",
+                labels_dist_fig
+                global_step=self.current_epoch,
+            )
+
+
+        self.val_labels.clear()
         self.val_conf_mat.reset()
         self.val_emb.clear()
 
---
+++ cmap/utils/distributions.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from typing import List
+
+
+def get_dist_plot(labels: List[str]):
+    labels_dist = pd.DataFrame(labels, columns=["label"]).value_counts()
+    # Create a bar plot with adjusted bar width
+    ax = labels_dist.plot.bar(
+        x="label", y="count", color="skyblue", edgecolor="black", width=0.8
+    )
+
+    # Set labels and title
+    ax.set_xlabel("Label", fontsize=12)
+    ax.set_title("Distribution of Labels", fontsize=16)
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45, ha="right")
+
+    # Add data labels on top of the bars
+    for p in ax.patches:
+        ax.annotate(
+            str(p.get_height()),
+            (p.get_x() + p.get_width() / 2.0, p.get_height()),
+            ha="center",
+            va="center",
+            xytext=(0, 10),
+            textcoords="offset points",
+        )
+
+    # Remove frame and ticks
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.get_yaxis().set_ticks([])
+
+    # Remove y-axis title
+    ax.set_ylabel("")
+
+    # Remove legend if it exists
+    ax.legend().set_visible(False)
+
+    # Display the plot
+    plt.tight_layout()
+
+    # Return the generated figure
+    return plt.gcf()
