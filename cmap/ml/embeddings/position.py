15c62df7e9d049cc92d82751e1ee4b66e1eba882
Christopher Salotti <christopher.salotti@gmail.com>
2023-12-07 19:06:55 +0100

    Config and classes for small dataset and mlp encoding/decoding

--- cmap/ml/embeddings/position.py
+++ cmap/ml/embeddings/position.py
@@ -5,14 +5,16 @@ import torch.nn as nn
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, n_head : int = 4, tau: float = 10000.0, max_len: int = 397):
+    def __init__(self, d_model: int, tau: float = 10000.0, max_len: int = 397):
         super().__init__()
 
         # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len + 1, d_model)
 
         position = torch.arange(0, max_len).float().unsqueeze(1)  # [max_len, 1]
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(tau)/ d_model))# [d_model/2,]
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(tau) / d_model)
+        )  # [d_model/2,]
 
         # keep pe[0,:] to zeros
         pe[1:, 0::2] = torch.sin(
@@ -25,4 +27,4 @@ class PositionalEncoding(nn.Module):
         self.encoding = nn.Embedding.from_pretrained(pe, freeze=True)
 
     def forward(self, positions):
-        return self.encoding(positions.int()) # [batch_size, seq_length, embed_dim]
+        return self.encoding(positions.int())  # [batch_size, seq_length, embed_dim]
